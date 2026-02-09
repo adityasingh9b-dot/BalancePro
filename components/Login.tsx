13d8edc53727073f8c6115c1c2a7638d33767831
Aditya Singh <acpedwardlivingston@gmail.com>
2026-02-09 17:03:52 +0530

    Update logo image class for rounded corners

--- components/Login.tsx
+++ components/Login.tsx
@@ -80,13 +80,9 @@ return (
 <img
   src="/assets/logo1.jpeg"
   alt="BalancePro"
-  className="
-    w-auto h-20 md:h-24 object-contain
-    rounded-xl
-    [mask-image:linear-gradient(to_right,transparent,black_15%,black_85%,transparent)]
-    [-webkit-mask-image:linear-gradient(to_right,transparent,black_15%,black_85%,transparent)]
-  "
+  className="w-auto h-20 md:h-24 object-contain rounded-lg"
 />
+
 
         </div>
         <p className="text-slate-400 text-[9px] font-bold uppercase tracking-[0.4em] opacity-80"> Contact Nitesh for Registration </p>
